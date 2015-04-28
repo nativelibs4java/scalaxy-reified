a32ec83297a7584953f70e6961fc4ab7ec6dc8bd
Olivier Chafik <olivier.chafik@gmail.com>
2015-04-27 22:33:45 -0700

    tweak macro paths

--- src/main/scala/scalaxy/reified/package.scala
+++ src/main/scala/scalaxy/reified/package.scala
@@ -27,7 +27,8 @@ package object reified {
    * which can be customized (by default, it handles constants, arrays, immutable collections,
    * tuples and options).
    */
-  implicit def reified[A: TypeTag](v: A): Reified[A] = macro internal.reifiedImpl[A]
+  implicit def reified[A: TypeTag](v: A): Reified[A] =
+    macro scalaxy.reified.internal.reifiedImpl[A]
 
   /**
    * Wrapper that provides Function1-like methods to a reified Function1 value.
@@ -140,11 +141,13 @@ package object reified {
   /**
    * Implicitly extract reified value from its wrappers (such as ReifiedFunction1, ReifiedFunction2).
    */
-  implicit def hasReifiedValueToReifiedValue[A: TypeTag](r: HasReified[A]): Reified[A] = macro internal.hasReifiedValueToReifiedValueImpl[A]
+  implicit def hasReifiedValueToReifiedValue[A: TypeTag](r: HasReified[A]): Reified[A] =
+    macro scalaxy.reified.internal.hasReifiedValueToReifiedValueImpl[A]
 
   /**
    * Implicitly convert reified value to their original non-reified value.
    */
-  implicit def hasReifiedValueToValue[A: TypeTag](r: HasReified[A]): A = macro internal.hasReifiedValueToValueImpl[A]
+  implicit def hasReifiedValueToValue[A: TypeTag](r: HasReified[A]): A =
+    macro scalaxy.reified.internal.hasReifiedValueToValueImpl[A]
 }
 
